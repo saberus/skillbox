6ccc0a552fc9c04cc573230e4110822028e83a7a
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-03-09 16:41:39 +0100

    added timer sprites and some logic

--- Assets/Scripts/Farm/Timer.cs
+++ Assets/Scripts/Farm/Timer.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float _maxTime = 5f;
+
+    public bool Tick = false;
+
+    private Image _img = null;
+    private float _currentTime = Mathf.Infinity;
+
+    private void Awake()
     {
-        
+        //find image in childrens
+        Transform timer = gameObject.transform.Find("Timer");
+        _img = timer.GetComponent<Image>();
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        _currentTime = _maxTime;
+    }
+
     void Update()
     {
-        
+        _currentTime -= Time.deltaTime;
+        Tick = false;
+        if (_currentTime <= 0)
+        {
+            Tick = true;
+        }
+        _img.fillAmount = _currentTime / _maxTime;
     }
 }
