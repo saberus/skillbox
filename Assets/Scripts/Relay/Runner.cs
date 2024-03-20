9737be412ac6bd37846cb3fab94205f3cea01992
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-03-20 14:32:57 +0100

    completed relay and runner

--- Assets/Scripts/Relay/Runner.cs
+++ Assets/Scripts/Relay/Runner.cs
@@ -5,22 +5,26 @@ public class Runner : MonoBehaviour
     [SerializeField] float _speed = 1f;
     [SerializeField] GameObject _stickPrefab = null;
     [SerializeField] GameObject _stickHandle = null;
+    [SerializeField] GameObject _runnerBody = null;
 
     private bool _isMoving = false;
     private bool _isStickVisible = false;
 
     private Vector3 _destination;
 
-    //private void Awake()
-    //{
-    //    _stickPrefab.SetActive(_isStickVisible);
-    //}
+    public int DestinationIndex = 0;
+
+    private void Awake()
+    {
+        _runnerBody.GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+    }
 
     private void Update()
     {
         if (!_isMoving) return;
         MoveRunner();
     }
+
     public void MoveRunner()
     {
         if (_destination == null) return;
@@ -32,8 +36,9 @@ public class Runner : MonoBehaviour
         return _destination;
     }
 
-    public void SetDestination(Vector3 destination)
+    public void SetDestination(int destinationIndex, Vector3 destination)
     {
+        DestinationIndex = destinationIndex;
         _destination = destination;
         transform.LookAt(destination);
     }
@@ -41,7 +46,6 @@ public class Runner : MonoBehaviour
     public void ToggleMove()
     {
         _isStickVisible = !_isStickVisible;
-        //_stickPrefab.SetActive(_isStickVisible);
         _isMoving = !_isMoving;
     }
 
@@ -53,4 +57,8 @@ public class Runner : MonoBehaviour
         _stickPrefab.transform.rotation = _stickHandle.transform.rotation;
     }
 
+    public GameObject GetStick()
+    {
+        return _stickPrefab;
+    }
 }
