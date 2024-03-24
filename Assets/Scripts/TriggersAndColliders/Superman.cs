7efe5529ba7b515bd1beada44b2eb1e7bd6c058f
sergey.prokofiev <sergey.prokofiev@jetbi.com>
2024-03-24 21:47:13 +0100

    added player, UI

--- Assets/Scripts/TriggersAndColliders/Superman.cs
+++ Assets/Scripts/TriggersAndColliders/Superman.cs
@@ -6,15 +6,17 @@ public class Superman : MonoBehaviour
 {
 
     [SerializeField] float _forceAmount = 0;
-    
+
+    Rigidbody _rb = null;
+
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        
+        Move();
     }
 
     void OnCollisionEnter(Collision collision)
@@ -23,5 +25,17 @@ public class Superman : MonoBehaviour
         {
             print("Contact");
         }
+    }
+
+    private void Move()
+    {
+        if (Input.GetKey(KeyCode.A))
+            _rb.AddForce(Vector3.left);
+        if (Input.GetKey(KeyCode.D))
+            _rb.AddForce(Vector3.right);
+        if (Input.GetKey(KeyCode.W))
+            _rb.AddForce(Vector3.forward);
+        if (Input.GetKey(KeyCode.S))
+            _rb.AddForce(Vector3.back);
     }
 }
